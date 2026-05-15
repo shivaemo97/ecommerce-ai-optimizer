583508308803e93560b59eb90a016b9fb61bd52d
shivaemo97 <shivaemo927@gmail.com>
2026-05-15 21:50:41 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body className="bg-slate-950 text-white min-h-screen selection:bg-indigo-500">
+        {children}
+      </body>
+    </html>
+  )
+}
